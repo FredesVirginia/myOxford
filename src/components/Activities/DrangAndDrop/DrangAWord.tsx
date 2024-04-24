036a4b234252a1daa4e1531efc570e5358861e1b
WilliamChampiopn <168019278+WilliamChampiopn@users.noreply.github.com>
2024-04-24 20:47:04 +0000

    casi

--- src/components/Activities/DrangAndDrop/DrangAWord.tsx
+++ src/components/Activities/DrangAndDrop/DrangAWord.tsx
@@ -63,6 +63,44 @@ const DraggableDiv: React.FC<IRequestGET> = ({ id, word }) => {
 };
 
 
+
+const DivContainer: React.FC<IRequestGET> = ({ id , word}) => {
+  const [droppedItem, setDroppedItem] = useState<string>();
+
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes,
+    drop: (item: any) => {
+      if (item.id === id) {
+        console.log(`Soltaste el div con ID ${id}`);
+        setDroppedItem(item.id);
+      }
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const isActive = canDrop && isOver;
+
+  return (
+    <div
+      ref={drop}
+      style={{
+        width: '300px',
+        height: '200px',
+        border: '2px dashed black',
+        padding: '20px',
+        position: 'relative',
+      }}
+    >
+      {isActive ? 'Suelta aquí' : 'Arrastra aquí'}
+      {droppedItem === id && <DraggableDiv word={word} id={id} />} {/* Renderizamos el div arrastrable si se soltó en este contenedor */}
+    </div>
+  );
+};
+
+
 const DrangAWord = () => {
 
   const [requestGET, setRequestGet] = useState<typeof INITIAL_DATA>(INITIAL_DATA)
@@ -89,7 +127,22 @@ const DrangAWord = () => {
           ))}
         </div>
       </center>
+
+     
      </center>
+
+     <div className="flex justify-center mt-20 space-x-5">
+      {questionParts.map((part, index) => (
+          <React.Fragment key={index}>
+            {/* Renderizar el texto de la parte */}
+            <p className="mr-2">{part}</p>
+            {/* Renderizar un div para el separador "_" */}
+            {index !== questionParts.length - 1 && (
+               <DivContainer id={index.toString()} word={part} />
+            )}
+          </React.Fragment>
+        ))}
+      </div>
     </DndProvider>
   )
 
