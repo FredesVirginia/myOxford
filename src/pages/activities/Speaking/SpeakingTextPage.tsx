21a6c31a4a08d00e60ccd95bac9e487be92b7cc6
totoSmith12 <168044011+totoSmith12@users.noreply.github.com>
2024-04-30 06:13:06 +0000

    falta activity

--- src/pages/activities/Speaking/SpeakingTextPage.tsx
+++ src/pages/activities/Speaking/SpeakingTextPage.tsx
@@ -1,4 +1,4 @@
-import { FaMicrophone, FaStop, FaPlay } from "react-icons/fa";
+import { FaMicrophone, FaStop, FaPlay , FaPause } from "react-icons/fa";
 import { IoMdPlay } from "react-icons/io";
 import { getColor } from "../../../helpers/getColor";
 import { GiLightningFrequency } from "react-icons/gi";
@@ -18,11 +18,18 @@ const SpeakingTextPage: React.FC<AudioRecorderProps> = ({ onRecord, onStop }) =>
     const audioChunksRef = useRef<Blob[]>([]);
     const audioRef = useRef<HTMLAudioElement | null>(null);
     const [mostrar, setMostrar] = useState(false);
-
-
+    const [parar ,  setParar] = useState(false);
+    const [play , setPlay] = useState(false);
     const handleMostrar = (value:boolean) => {
 
         setMostrar(value);
+        setIsRecording(!isRecording);
+    }
+
+    const handleParar = (value:boolean) => {
+
+        setParar(value);
+        handleStop();
     }
 
     useEffect(() => {
@@ -59,7 +66,14 @@ const SpeakingTextPage: React.FC<AudioRecorderProps> = ({ onRecord, onStop }) =>
         }
     };
 
+  const handlePlay2 = (value:boolean) =>{
+   setIsRecording(false)
+        setPlay(value);
+       
+        handlePlay();
+  }
     const handleStop = () => {
+     setPlay(false);
         if (audioRef.current && audioRef.current.paused === false) {
             audioRef.current.pause();
         }
@@ -67,48 +81,18 @@ const SpeakingTextPage: React.FC<AudioRecorderProps> = ({ onRecord, onStop }) =>
 
     return (
         <div>
-            <div className="p-4">
-                <div className="flex items-center justify-center">
-                    <button
-                        className={`${isRecording ? 'bg-red-500' : 'bg-blue-500'} text-white font-bold py-2 px-4 rounded mr-2`}
-                        onClick={() => setIsRecording(!isRecording)}
-                    >
-                        {isRecording ? 'Stop Recording' : 'Start Recording'}
-                    </button>
-                    <button
-                        className="bg-green-500 text-white font-bold py-2 px-4 rounded mr-2"
-                        onClick={handlePlay}
-                    >
-                        Play
-                    </button>
-                    <button
-                        className="bg-gray-500 text-white font-bold py-2 px-4 rounded mr-2"
-                        onClick={handleStop}
-                    >
-                        Stop
-                    </button>
-                    <button
-                        className="bg-yellow-500 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => setIsRecording(false)}
-                    >
-                        Stop Recording
-                    </button>
-                </div>
-                {audioChunksRef.current.length > 0 && (
-                    <>
-                        <audio ref={audioRef} className="w-full mt-4 bg-red-700" />
-                    </>
-                )}
-                {!isRecording && audioChunksRef.current.length === 0 && (
-                    <p className="text-center mt-4">No audio recorded.</p>
-                )}
-            </div>
+           
 
 
-            <div className="bg-red-400 py-4 px-3 flex spca-x-10 justify-center">
+            <div className=" py-4 px-3 flex spca-x-10 justify-center">
                 <div className="flex flex-col items-center space-y-2">
                    {mostrar ? (
-                        <GiLightningFrequency   style={{ backgroundColor: getColor("my-speaking"), color: "white", borderRadius: "100%" }}
+                        <GiLightningFrequency  
+                        onClick={() => handleMostrar(false)}
+                         style={{
+                             backgroundColor: parar ? getColor("my-speaking") : getColor("my-speaking-300") ,
+                              color: "white", 
+                              borderRadius: "100%" }}
                         className="  text-4xl fill-white w-[80px] h-[80px] p-3 "/>
                     ) : (
                         <FaMicrophone onClick={() => handleMostrar(true)}  style={{ backgroundColor: getColor("my-speaking"), color: "white", borderRadius: "100%" }}
@@ -116,20 +100,64 @@ const SpeakingTextPage: React.FC<AudioRecorderProps> = ({ onRecord, onStop }) =>
                     )}
 
                     <button  
-                        onClick={()=>handleMostrar(false)}
-                     style={{ border: "solid", borderColor: getColor("my-speaking"), color: getColor("my-speaking") }} className="bg-white ">
+                        onClick={() => handleParar(true)}
+                        style={{
+                             border: "solid", 
+                             borderColor: getColor("my-speaking"), 
+                             color: getColor("my-speaking"),
+                             backgroundColor: parar ? "white" : (mostrar ? getColor("my-speaking-300") : "white") , 
+                             ...(!mostrar && { backgroundColor: 'white'})
+                             }}
+                             
+                            className="bg-white hover:bg-white "
+                     >
                         Stop to Speaking
                     </button>
                 </div>
 
 
                 <div className="flex flex-col items-center space-y-2">
-                    <IoMdPlay style={{ backgroundColor: getColor("my-speaking"), color: "white", borderRadius: "100%" }} className="  text-4xl fill-white w-[70px] h-[80px] p-3 " />
-                    <button style={{ border: "solid", borderColor: getColor("my-speaking"), color: getColor("my-speaking") }} className="bg-white ">
+                   
+                   
+                     {
+                            play ? (
+                              <FaPause 
+                              onClick={ handleStop}
+                               style={{ 
+                               backgroundColor: play ?  getColor("my-speaking-300") : "white", 
+                               color: "white", borderRadius: "50%" }} 
+                               className="  text-sm fill-white w-[80px] h-[80px] p-5 " 
+                               
+                               />
+
+                              
+                            ) : (
+                             <IoMdPlay style={{ backgroundColor: getColor("my-speaking"), color: "white", borderRadius: "100%" }} className=" pl-5 text-4xl fill-white w-[80px] h-[80px] p-3 " />
+                            )
+                     }
+                    <button 
+                    onClick={() =>handlePlay2(true)}
+                    style={{ 
+                    backgroundColor : parar ? getColor("my-speaking-300") : "white" ,
+                    border: "solid",
+                     borderColor: getColor("my-speaking"), 
+                     color: getColor("my-speaking") 
+                     }} className="bg-white ml-5 ">
                         Hear your Record
                     </button>
                 </div>
+
+              
             </div>
+
+               {audioChunksRef.current.length > 0 && (
+                    <>
+                        <audio ref={audioRef} className="w-full mt-4 bg-red-700" />
+                    </>
+                )}
+                {!isRecording && audioChunksRef.current.length === 0 && (
+                    <p className="text-center mt-4">No audio recorded.</p>
+                )}
         </div>
     );
 };
