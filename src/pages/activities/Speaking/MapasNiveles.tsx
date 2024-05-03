85d2bc515da1e91ab88a83c14f5458994f2258a6
totoSmith12 <168044011+totoSmith12@users.noreply.github.com>
2024-05-03 04:50:08 +0000

    mapa casi listo

--- src/pages/activities/Speaking/MapasNiveles.tsx
+++ src/pages/activities/Speaking/MapasNiveles.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "../../../App.css";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
+import { FaArrowLeft } from "react-icons/fa";
 const MapasNiveles = () => {
 
     const [rotation, setRotation] = useState(0);
@@ -12,20 +15,25 @@ const MapasNiveles = () => {
         setRotation(prevRotation => prevRotation - 45);
     };
     return (
-        <div className="bg-red-300 h-[825px] overflow-hidden flex justify-center">
+        <div className=" fondo  h-[825px] overflow-hidden flex justify-center">
 
 
            
 
 
 
-            <div className="container bg-red-900">
+            <div className="container bg-red-900 flex justify-center">
                 <div className="image-container" style={{ transform: `rotate(${rotation}deg)` }}>
                     <img src="../../../img/Capa 1.png " alt="Imagen" className="w-[1000px]" />
                 </div>
-                <div className="buttons  flex justify-center">
-                    <button onClick={rotateLeft}>Izquierda</button>
-                    <button onClick={rotateRight}>Derecha</button>
+
+                <div className="image-container2" >
+                    <img src="../../../img/homeTown.png" alt="Imagen" className="w-[340px]" />
+                </div>
+
+                <div className="  image-container3 buttons  flex justify-center space-x-[360px]">
+                    <button  style={{padding: "0px 0px"}} onClick={rotateLeft}> <img src="../../../img/left.png" alt="Imagen" className="w-[160px]" /> </button>
+                    <button style={{padding: "0px 0px"}} onClick={rotateRight}> <img src="../../../img/right.png" alt="Imagen" className="w-[160px]" /> </button>
                 </div>
             </div>
 
